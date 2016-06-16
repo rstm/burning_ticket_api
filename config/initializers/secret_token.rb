a5e78f2d0d8b7662627c0c3e0550f13d4037baf5
Rustem Mazitov <rmazitov@d-l-s.ru>
2016-06-16 20:43:33 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BurningTicket::Application.config.secret_key_base = '69746aaf3d92852bae6c79ed3f30e7c7da70e11a3979ab58797b3a580bca617f67ce0dfa1a2144417437bcd11d783e23a9c8f46de5d35995b331901a89e9c273'
